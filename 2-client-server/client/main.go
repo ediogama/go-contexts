0fb897434bc63ab9eb1390258e05e85943efae87
Edio Gama <ediogama@hotmail.com>
2023-01-14 21:37:38 -0300

    add client with context

---
+++ 2-client-server/client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
+}
--- 2-client-server/server/main.go
+++ 2-client-server/server/main.go
@@ -7,14 +7,20 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", handler)
+
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
 	ctx := r.Context()
+
 	log.Println("Request iniciada")
+
 	defer log.Println("Request finalizada")
+
 	select {
 	case <-time.After(5 * time.Second):
 		log.Println("Request processada com sucesso")
